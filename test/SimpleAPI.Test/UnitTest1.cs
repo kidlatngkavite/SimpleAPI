933ccb3fcd9937d1ac444889cf79ece07f704d10
Ferdinand Escolar <isko@Ferdinands-MacBook-Pro.local>
2020-03-11 17:21:21 +0800

    Initial Commit

---
+++ test/SimpleAPI.Test/UnitTest1.cs
@@ -0,0 +1,22 @@
+using System;
+using Xunit;
+using SimpleAPI.Controllers;
+
+namespace SimpleAPI.Test
+{
+    public class UnitTest1
+    {
+        WeatherForecastController Isko = new WeatherForecastController();
+        [Fact]
+        public void GetReturnsMyName()
+        {
+            var returnValue = Isko.Get(1);
+            Assert.Equal("Ferdinand Escolar",returnValue.Value);
+        }
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
