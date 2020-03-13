32924fa7f0a14e18f92f4be2fbc0a0cfdb062f68
kidlatngkavite <ferdinand.escolar@itsmeph.com>
2020-03-13 13:29:19 +0800

    sana gumana na

--- test/SimpleAPI.Test/UnitTest1.cs
+++ test/SimpleAPI.Test/UnitTest1.cs
@@ -11,7 +11,7 @@ namespace SimpleAPI.Test
         public void GetReturnsMyName()
         {
             var returnValue = Isko.Get(1);
-            Assert.Equal("Ferdinand Escolars",returnValue.Value);
+            Assert.Equal("Ferdinand Escolar",returnValue.Value);
         }
         [Fact]
         public void Test1()
